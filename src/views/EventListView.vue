4f36bfaa4f7d2d755e9efba321f1cfeca210fa12
chawanakorns <chawanakorn_s@cmu.ac.th>
2024-08-12 17:13:30 +0700

    Lab 5 (2.3)

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -51,7 +51,7 @@ function updatePageSize(newSize: number) {
     Page size:
     <input type="number" v-model.number="pageSize" @change="updatePageSize(pageSize)" />
   </div>
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <RouterLink id="page-prev" :to="{ name: 'event-list-view', query: { page: page - 1, size: pageSize } }" rel="prev"
@@ -63,12 +63,6 @@ function updatePageSize(newSize: number) {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 .pagination {
   display: flex;
   width: 290px;
